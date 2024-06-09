8fb250dcb85d7daa11c6df73fdf8e51ef195e1e1
Pradnya <xyz@example.com>
2024-06-09 20:38:26 +0530

    new changes in feature

--- testnew1.js
+++ testnew1.js
@@ -1,4 +1,5 @@
 <script type="text/javascript">  
 document.write("Hi I am learning JS. JavaScript is a simple language for javatpoint learners");  
 document.write("feature branch");
+<p>some new changes</p>
 </script>  
